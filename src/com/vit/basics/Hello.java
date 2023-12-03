fb1b577b64df8774b133dcfc9d781e3ba67a6e2b
vitschool92 <vitschool92@gmail.com>
2023-12-03 14:46:42 +0530

    intial commit

---
+++ src/com/vit/basics/Hello.java
@@ -0,0 +1,11 @@
+package com.vit.basics;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		System.out.println("\n My first program");
+		System.out.println("This my github class");
+		
+	}
+}
